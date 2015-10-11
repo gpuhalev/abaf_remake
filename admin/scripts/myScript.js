8133ad9b75fa30578eb421cbe77b8747ef51e1fb
gpuhalev <gesho.puh@gmail.com>
2015-10-11 20:51:54 +0200

    adding admin beginning

---
+++ admin/scripts/myScript.js
@@ -0,0 +1,16 @@
+$(function() {
+
+
+	/*HIDDEN LEFT MENU*/
+	/*var titleLeftMenu = $('#leftMenu #title');
+	var contentLeftMenu = $('#leftMenu #content');
+	$(titleLeftMenu).click(function(){
+		var myName = $(this).attr("name");
+		var myElement = $('#leftMenu #content' + '[name=' + myName + ']');
+		if(myElement.css('display')=='none'){
+			myElement.show("fast", "swing");
+		}else{
+			myElement.hide("fast", "swing");
+		}
+	});*/
+});
